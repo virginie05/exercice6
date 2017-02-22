f76f68a15b2c18247ff2fbb626677c0547ed973a
Virginie <leguilcher.virgie@outlook.fr>
2017-02-22 10:56:31 +0100

    nombre

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
-  function affichage(){
-  var first = document.getElementById('premier_nombre').value;
-  var second = document.getElementById('deuxieme_nombre').value;
-  var nombre1 = parseInt(first);
-  var nombre2 = parseFloat(second);
-  var nombre3 = nombre1 % nombre2;
-  alert(nombre3);
-  }
+function modulo(){
+var number1 = document.getElementById("premier_nombre").value;
+var number2 = document.getElementById("deuxieme_nombre").value;
+if (isNaN(number1) || isNaN(number2)){ // Si la valeur n'est pas un nombre: alert('Il faut des nombres')
+ alert('Il faut des nombres');
+} else if(number2 == 0){
+   alert('On ne peut pas diviser par zéro');
+} else { // Sinon tu me donne le résultat de la multiplication
+   var result = parseInt(number1) % parseFloat(number2);
+   alert(result);
+ }
+}
